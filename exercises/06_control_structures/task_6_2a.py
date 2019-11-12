6a193accaa3e9b750e14e21b526b85e227fba5b8
vdm-nv <55355810+vdm-nv@users.noreply.github.com>
2019-11-12 12:44:32 +0200

    Update task 6_2a

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -3,48 +3,48 @@
 '''
 Задание 6.2a
 '''
-   
-
-
 add = input('Введите IP адрес: ')
 
 bb = add.split('.')
-
-if add.count('.') != 3:
-    print('please input digits by dots')
-    for did in bb:
-        if did.isdigit() != True:
-            print('Please input only digits')
-            
+          
 octets = []
+broad = ['255','255','255','255']
+unass = ['0', '0', '0', '0']
 
-broad = [255, 255, 255, 255]
-
-unass = [0, 0, 0, 0]
-
-
+for did in bb:
+  if did.isdigit() != True:
+    print('\n'+ '-'*30)
+    print('Please input only digits')
+    break
+    print('-'*30)
 
 for addr in bb:
-    octets.append(int(addr))
-
-
-if int(bb[0]) <= 223:
+    octets.append((addr))
+    
+while True:
+  if add.count('.') != 3:
+    print('\n'+ '-'*30)
+    print('please input digits by dots')
+    break
+    print('-'*30)
+  elif int(bb[0]) > 0 and int(bb[0]) <= 223:
     print('\n'+ '-'*30)
     print('Это unicast')
     print('-'*30)
-elif int(bb[0]) <= 239:
+  elif int(bb[0]) >= 224 and int(bb[0])<= 239:
     print('\n'+ '-'*30)
     print('Это multicast')
     print('\n'+ '-'*30)
-elif octets == broad:
+  elif octets == broad:
     print('\n'+ '-'*30)
     print('Это local broadcast')
     print('-'*30)
-elif octets == unass:
+  elif octets == unass:
     print('\n'+ '-'*30)
     print('Это unassagnet')
     print('-'*30)
-else:
+  else:
     print('\n'+ '-'*30)
     print('Это unused')
     print('-'*30)
+  break
