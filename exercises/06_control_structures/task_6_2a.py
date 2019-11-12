87c48266206dee2c19fab926af75e444f5d345aa
vdm-nv <55355810+vdm-nv@users.noreply.github.com>
2019-11-12 13:52:28 +0200

    update 02

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -11,40 +11,40 @@ octets = []
 broad = ['255','255','255','255']
 unass = ['0', '0', '0', '0']
 
-for did in bb:
-  if did.isdigit() != True:
+for dig in bb:
+  if dig.isdigit() != True or add.count('.') != 3:
     print('\n'+ '-'*30)
     print('Please input only digits')
     break
     print('-'*30)
 
 for addr in bb:
-    octets.append((addr))
-    
+    if int(addr) > 0 and int(addr) <= 255:
+          octets.append((addr))
+    else:
+      print('\n'+ '-'*30)
+      print('Please input only digit in octet from 0 to 255')
+      break
+
 while True:
-  if add.count('.') != 3:
-    print('\n'+ '-'*30)
-    print('please input digits by dots')
+    if int(bb[0]) > 0 and int(bb[0]) <= 223:
+      print('\n'+ '-'*30)
+      print('Это unicast')
+      print('-'*30)
+    elif int(bb[0]) >= 224 and int(bb[0])<= 239:
+      print('\n'+ '-'*30)
+      print('Это multicast')
+      print('\n'+ '-'*30)
+    elif octets == broad:
+      print('\n'+ '-'*30)
+      print('Это local broadcast')
+      print('-'*30)
+    elif octets == unass:
+      print('\n'+ '-'*30)
+      print('Это unassagnet')
+      print('-'*30)
+    else:
+      print('\n'+ '-'*30)
+      print('Это unused')
+      print('-'*30)
     break
-    print('-'*30)
-  elif int(bb[0]) > 0 and int(bb[0]) <= 223:
-    print('\n'+ '-'*30)
-    print('Это unicast')
-    print('-'*30)
-  elif int(bb[0]) >= 224 and int(bb[0])<= 239:
-    print('\n'+ '-'*30)
-    print('Это multicast')
-    print('\n'+ '-'*30)
-  elif octets == broad:
-    print('\n'+ '-'*30)
-    print('Это local broadcast')
-    print('-'*30)
-  elif octets == unass:
-    print('\n'+ '-'*30)
-    print('Это unassagnet')
-    print('-'*30)
-  else:
-    print('\n'+ '-'*30)
-    print('Это unused')
-    print('-'*30)
-  break
