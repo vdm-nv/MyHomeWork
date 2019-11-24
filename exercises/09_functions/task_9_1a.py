c0bc5efbc546ce3bfc2026e90492902fd0552d3c
vdm-nv <55355810+vdm-nv@users.noreply.github.com>
2019-11-24 20:04:25 +0200

    Create task_9_1a.py

---
+++ exercises/09_functions/task_9_1a.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 9.1a
+
+Сделать копию функции из задания 9.1.
+
+Дополнить скрипт:
+* ввести дополнительный параметр, который контролирует будет ли настроен port-security
+ * имя параметра 'psecurity'
+ * по умолчанию значение None
+ * для настройки port-security, как значение надо передать список команд port-security (находятся в списке port_security_template)
+
+Функция должна возвращать список всех портов в режиме access
+с конфигурацией на основе шаблона access_mode_template и шаблона port_security_template, если он был передан.
+В конце строк в списке не должно быть символа перевода строки.
+
+
+Проверить работу функции на примере словаря access_config,
+с генерацией конфигурации port-security и без.
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+
+'''
+
+access_mode_template = [
+    'switchport mode access', 'switchport access vlan',
+    'switchport nonegotiate', 'spanning-tree portfast',
+    'spanning-tree bpduguard enable'
+]
+
+port_security_template = [
+    'switchport port-security maximum 2',
+    'switchport port-security violation restrict',
+    'switchport port-security'
+]
+
+access_config = {
+    'FastEthernet0/12': 10,
+    'FastEthernet0/14': 11,
+    'FastEthernet0/16': 17
+}
+
+result = []
+
+def generate_access_config(intf_vlan_mapping, access_template, psecurity = None):
+    for intf in intf_vlan_mapping:
+        result.append('interface {}'.format(intf))
+        for command in access_template:
+            if command.endswith('access vlan'):
+                result.append('{} {}'.format(command, intf_vlan_mapping[intf]))
+            else:
+                result.append('{}'.format(command))
+        if psecurity is None:
+            pass
+        else:
+            for comm in psecurity:
+                result.append('{}'.format(comm))
+    return result
