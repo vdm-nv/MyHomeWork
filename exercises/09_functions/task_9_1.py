fb3746e33dccf9fd208ac4591c32c83b19606a06
vdm-nv <55355810+vdm-nv@users.noreply.github.com>
2019-11-24 20:03:14 +0200

    Create task_9_1.py

---
+++ exercises/09_functions/task_9_1.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 9.1
+
+Создать функцию, которая генерирует конфигурацию для access-портов.
+
+Функция ожидает такие аргументы:
+
+- словарь с соответствием интерфейс-VLAN такого вида:
+    {'FastEthernet0/12':10,
+     'FastEthernet0/14':11,
+     'FastEthernet0/16':17}
+- шаблон конфигурации access-портов в виде списка команд (список access_mode_template)
+
+Функция должна возвращать список всех портов в режиме access
+с конфигурацией на основе шаблона access_mode_template.
+В конце строк в списке не должно быть символа перевода строки.
+
+В этом задании заготовка для функции уже сделана и надо только продолжить писать само тело функции.
+
+
+Пример итогового списка (перевод строки после каждого элемента сделан для удобства чтения):
+[
+'interface FastEthernet0/12',
+'switchport mode access',
+'switchport access vlan 10',
+'switchport nonegotiate',
+'spanning-tree portfast',
+'spanning-tree bpduguard enable',
+'interface FastEthernet0/17',
+'switchport mode access',
+'switchport access vlan 150',
+'switchport nonegotiate',
+'spanning-tree portfast',
+'spanning-tree bpduguard enable',
+...]
+
+Проверить работу функции на примере словаря access_config.
+
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+
+'''
+
+access_mode_template = [
+    'switchport mode access', 'switchport access vlan',
+    'switchport nonegotiate', 'spanning-tree portfast',
+    'spanning-tree bpduguard enable'
+]
+
+access_config = {
+    'FastEthernet0/12': 10,
+    'FastEthernet0/14': 11,
+    'FastEthernet0/16': 17
+}
+
+
+def generate_access_config(intf_vlan_mapping, access_template):
+    result = []
+    for intf in intf_vlan_mapping:
+        result.append('interface {}'.format(intf))
+        for command in access_template:
+            if command.endswith('access vlan'):
+                result.append('{} {}'.format(command, intf_vlan_mapping[intf]))
+            else:
+                result.append('{}'.format(command))
+    return result
