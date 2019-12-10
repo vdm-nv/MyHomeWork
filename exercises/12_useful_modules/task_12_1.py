271ab515362c3a46799cbf714898f77f9f793bd8
vdm-nv <vpnovik@gmail.com>
2019-12-10 13:42:57 +0200

    Add task 12

---
+++ exercises/12_useful_modules/task_12_1.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 12.1
+
+Создать функцию ping_ip_addresses, которая проверяет доступность IP-адресов.
+
+Функция ожидает как аргумент список IP-адресов.
+
+Функция должна возвращать кортеж с двумя списками:
+* список доступных IP-адресов
+* список недоступных IP-адресов
+
+Для проверки доступности IP-адреса, используйте ping.
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+'''
+
+import subprocess
+
+res = ()
+reach = []
+unreach = []
+
+def ping_ip_addresses(ip_address):
+    for ip in ip_address:
+        print('Now we are try to reach', ip)
+        result = subprocess.run('ping {} -c 2 -n'.format(ip),
+                                shell=True,stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE, encoding='utf-8')
+        if result.returncode == 0:
+            reach.append(ip)
+            #print('\n its working IP', reach)
+        else:
+            unreach.append(ip)
+            #print('\n IP is unreachable', unreach)
+    res = (reach,unreach)
+    return res
+
