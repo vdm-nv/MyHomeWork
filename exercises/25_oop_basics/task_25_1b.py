3cf536eb571fc516b3ae0c341fd417eb8659ee54
vdm-nv <vpnovik@gmail.com>
2020-03-25 15:57:23 +0200

    Done two exercises 25.1b and c

---
+++ exercises/25_oop_basics/task_25_1b.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+'''
+Задание 25.1b
+
+Изменить класс Topology из задания 25.1a или 25.1.
+
+Добавить метод delete_link, который удаляет указанное соединение.
+Метод должен удалять и зеркальное соединение, если оно есть.
+
+Если такого соединения нет, выводится сообщение "Такого соединения нет".
+
+Создание топологии
+In [7]: t = Topology(topology_example)
+
+In [8]: t.topology
+Out[8]:
+{('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
+ ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
+ ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
+ ('R3', 'Eth0/0'): ('SW1', 'Eth0/3'),
+ ('R3', 'Eth0/1'): ('R4', 'Eth0/0'),
+ ('R3', 'Eth0/2'): ('R5', 'Eth0/0')}
+
+Удаление линка:
+In [9]: t.delete_link(('R3', 'Eth0/1'), ('R4', 'Eth0/0'))
+
+In [10]: t.topology
+Out[10]:
+{('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
+ ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
+ ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
+ ('R3', 'Eth0/0'): ('SW1', 'Eth0/3'),
+ ('R3', 'Eth0/2'): ('R5', 'Eth0/0')}
+
+Удаление зеркального линка
+In [11]: t.delete_link(('R5', 'Eth0/0'), ('R3', 'Eth0/2'))
+
+In [12]: t.topology
+Out[12]:
+{('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
+ ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
+ ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
+ ('R3', 'Eth0/0'): ('SW1', 'Eth0/3')}
+
+Если такого соединения нет, выводится сообщение:
+In [13]: t.delete_link(('R5', 'Eth0/0'), ('R3', 'Eth0/2'))
+Такого соединения нет
+
+'''
+from pprint import pprint
+
+topology_example = {('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
+                    ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
+                    ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
+                    ('R3', 'Eth0/0'): ('SW1', 'Eth0/3'),
+                    ('R3', 'Eth0/1'): ('R4', 'Eth0/0'),
+                    ('R3', 'Eth0/2'): ('R5', 'Eth0/0'),
+                    ('SW1', 'Eth0/1'): ('R1', 'Eth0/0'),
+                    ('SW1', 'Eth0/2'): ('R2', 'Eth0/0'),
+                    ('SW1', 'Eth0/3'): ('R3', 'Eth0/0')}
+class Topology:
+    def __init__(self, top):
+        self.topology = self._normalize(top)
+
+    def _normalize(self, top):
+         self.res = {}
+         for key,value in top.items():
+             if value not in self.res.keys():
+                 self.res[key] = value
+         return self.res
+
+    def delete_link(self, port01, port02):
+        flag = True
+        for k,v in list(self.res.items()):
+            if port01 == k and port02 == v or port02 == k and port01 == v:
+                self.res.pop(k)
+                flag = False
+        if flag:
+            print('Такого соединения нет')
+
+if __name__ == "__main__":
+    top = Topology(topology_example)
+    pprint(top.topology)
+    top.delete_link(('R3', 'Eth0/2'), ('R5', 'Eth0/0'))
+    print("Removing ('R3', 'Eth0/2'), ('R5', 'Eth0/0')")
+    pprint(top.topology)
+    print("Try to remove again ('R3', 'Eth0/2'), ('R5', 'Eth0/0')")
+    top.delete_link(('R3', 'Eth0/2'), ('R5', 'Eth0/0')) 
---
+++ exercises/25_oop_basics/task_25_1c.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+'''
+Задание 25.1c
+
+Изменить класс Topology из задания 25.1b.
+
+Добавить метод delete_node, который удаляет все соединения с указаным устройством.
+
+Если такого устройства нет, выводится сообщение "Такого устройства нет".
+
+Создание топологии
+In [1]: t = Topology(topology_example)
+
+In [2]: t.topology
+Out[2]:
+{('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
+ ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
+ ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
+ ('R3', 'Eth0/0'): ('SW1', 'Eth0/3'),
+ ('R3', 'Eth0/1'): ('R4', 'Eth0/0'),
+ ('R3', 'Eth0/2'): ('R5', 'Eth0/0')}
+
+Удаление устройства:
+In [3]: t.delete_node('SW1')
+
+In [4]: t.topology
+Out[4]:
+{('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
+ ('R3', 'Eth0/1'): ('R4', 'Eth0/0'),
+ ('R3', 'Eth0/2'): ('R5', 'Eth0/0')}
+
+Если такого устройства нет, выводится сообщение:
+In [5]: t.delete_node('SW1')
+Такого устройства нет
+
+'''
+from pprint import pprint
+
+topology_example = {('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
+                    ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
+                    ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
+                    ('R3', 'Eth0/0'): ('SW1', 'Eth0/3'),
+                    ('R3', 'Eth0/1'): ('R4', 'Eth0/0'),
+                    ('R3', 'Eth0/2'): ('R5', 'Eth0/0'),
+                    ('SW1', 'Eth0/1'): ('R1', 'Eth0/0'),
+                    ('SW1', 'Eth0/2'): ('R2', 'Eth0/0'),
+                    ('SW1', 'Eth0/3'): ('R3', 'Eth0/0')}
+
+class Topology:
+    def __init__(self, top):
+        self.topology = self._normalize(top)
+
+    def _normalize(self, top):
+        self.res = {}
+        for key,value in top.items():
+            if value not in self.res.keys():
+                self.res[key] = value
+        return self.res
+
+    def delete_node(self, node):
+        flag = True
+
+        for k,v in list(self.res.items()):
+            if node in k[0] or node in v[0]:
+                self.res.pop(k)
+                flag = False
+        if flag:
+            print(f'Такого устройства нет {node}')
+
+if __name__ == "__main__":
+    top = Topology(topology_example)
+    pprint(top.topology)
+    top.delete_node('R3')
+    print("Removing node")
+    pprint(top.topology)
+    print("Try to remove again 'R3'")
+    top.delete_node('R3')
