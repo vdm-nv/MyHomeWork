684be13b9a380851a02bdfd9cf1139019266e7c2
vdm-nv <55355810+vdm-nv@users.noreply.github.com>
2020-04-02 12:13:33 +0300

    update describtion

--- exercises/25_oop_basics/task_25_2c.py
+++ exercises/25_oop_basics/task_25_2c.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
 '''
-Задание 25.2b
+Скопировать класс CiscoTelnet из задания 25.2b и изменить метод send_config_commands добавив проверку команд на ошибки.
 
-Скопировать класс CiscoTelnet из задания 25.2a и добавить метод send_config_commands.
+У метода send_config_commands должен быть дополнительный параметр strict:
 
-
-Метод send_config_commands должен уметь отправлять одну команду конфигурационного режима или список команд.
-Метод должен возвращать вывод аналогичный методу send_config_set у netmiko (пример вывода ниже).
+strict=True значит, что при обнаружении ошибки, необходимо сгенерировать исключение ValueError
+strict=False значит, что при обнаружении ошибки, надо только вывести на стандартный поток вывода сообщене об ошибке
+Метод дожен возвращать вывод аналогичный методу send_config_set у netmiko (пример вывода ниже). Текст исключения и ошибки в примере ниже.
 
 Пример создания экземпляра класса:
-In [1]: from task_25_2b import CiscoTelnet
+In [1]: from task_25_2c import CiscoTelnet
 
 In [2]: r1_params = {
    ...:     'ip': '192.168.100.1',
@@ -20,13 +20,40 @@ In [2]: r1_params = {
 
 In [3]: r1 = CiscoTelnet(**r1_params)
 
+In [4]: commands_with_errors = ['logging 0255.255.1', 'logging', 'i']
+In [5]: correct_commands = ['logging buffered 20010', 'ip http server']
+In [6]: commands = commands_with_errors+correct_commands
+
 Использование метода send_config_commands:
+In [7]: print(r1.send_config_commands(commands, strict=False))
+При выполнении команды "logging 0255.255.1" на устройстве 192.168.100.1 возникла ошибка -> Invalid input detected at '^' marker.
+При выполнении команды "logging" на устройстве 192.168.100.1 возникла ошибка -> Incomplete command.
+При выполнении команды "i" на устройстве 192.168.100.1 возникла ошибка -> Ambiguous command:  "i"
+conf t
+Enter configuration commands, one per line.  End with CNTL/Z.
+R1(config)#logging 0255.255.1
+                   ^
+% Invalid input detected at '^' marker.
 
-In [5]: r1.send_config_commands('logging 10.1.1.1')
-Out[5]: 'conf t\r\nEnter configuration commands, one per line.  End with CNTL/Z.\r\nR1(config)#logging 10.1.1.1\r\nR1(config)#end\r\nR1#'
+R1(config)#logging
+% Incomplete command.
 
-In [6]: r1.send_config_commands(['interface loop55', 'ip address 5.5.5.5 255.255.255.255'])
-Out[6]: 'conf t\r\nEnter configuration commands, one per line.  End with CNTL/Z.\r\nR1(config)#interface loop55\r\nR1(config-if)#ip address 5.5.5.5 255.255.255.255\r\nR1(config-if)#end\r\nR1#'
+R1(config)#i
+% Ambiguous command:  "i"
+R1(config)#logging buffered 20010
+R1(config)#ip http server
+R1(config)#end
+R1#
+
+In [8]: print(r1.send_config_commands(commands, strict=True))
+---------------------------------------------------------------------------
+ValueError                                Traceback (most recent call last)
+<ipython-input-8-0abc1ed8602e> in <module>
+----> 1 print(r1.send_config_commands(commands, strict=True))
+
+...
+
+ValueError: При выполнении команды "logging 0255.255.1" на устройстве 192.168.100.1 возникла ошибка -> Invalid input detected at '^' marker.
 
 '''
 import textfsm,clitable
